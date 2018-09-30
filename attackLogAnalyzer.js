e36639990100acdbc6e719fd27f90f6813e17824
Tamas Peter <falconsaglevlist@gmail.com>
2018-09-30 13:55:50 +0200

    display attackers/attackers lost icons

--- attackLogAnalyzer.js
+++ attackLogAnalyzer.js
@@ -15,109 +15,140 @@
 (function () {
 
     function addJQuery(callback) {
-  var script = document.createElement("script");
-  script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
-}
+        var script = document.createElement("script");
+        script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
+        script.addEventListener('load', function () {
+            var script = document.createElement("script");
+            script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+            document.body.appendChild(script);
+        }, false);
+        document.body.appendChild(script);
+    }
 
 
     // the guts of this userscript
-function main() {
+    function main() {
 
-     var btnAnalyze = $('<input type="button" id="btnAnalyze" value="Analyze reports" />');
-     jQuery("div.boxes-contents.cf:first").append(btnAnalyze);
-     jQuery("#btnAnalyze").on('click',function(){
-         AnalyzeReports();
-     });
-
-
-    //wrap jquery dom element in html and log
-     function logJqueryDOM( domElement){
-         console.log(domElement.clone().wrap('<div/>').parent().html())
-     }
-
-     function AnalyzeReports(){
-      jQuery('td#logAnalyzerTD').each(function(){
-      $(this).remove();
-      });
-     var no=0;
-     //minden kifosztotta, és támad linkre
-    jQ("a:contains('kifosztotta'),a:contains('támad')")
-        .each(function() {
-
-
-        var linkToReport =$(this).attr('href');
-        var result = null;
-        jQuery.ajax({
-           url: linkToReport,
-           type: 'get',
-           dataType: 'html',
-           async: false,
-           success: function(data) {
-            var sumStolen = 0
-            var parsedHtml = jQuery.parseHTML(data);
-
-            jQuery("table#attacker tbody.goods img",parsedHtml).filter( ".r1,.r2,.r3,.r4" ).each(function(){
-                sumStolen += Number($(this).parent().text());
-            });
-            result = sumStolen+";";
-
-            //numbers of first 5 attacker type like (5,0,0) = 5 buzogányos
-            var attackers = jQuery("table#attacker tbody.units:nth-child(3) td",parsedHtml).slice(0,5)
-               .map(function(){
-                return $(this).text()
-            })
-            .get()
-            .join(",");
-
-             var sumAttackerLost = 0
-             jQuery("table#attacker tbody.units.last td",parsedHtml).each(function(){
-                 sumAttackerLost += Number($(this).text());
-             });
-
-
-             var sumDefenderLost =0;
-             jQuery("table#attacker",parsedHtml).siblings("table").find("tbody.units.last td").each(function(){
-                 sumDefenderLost += Number($(this).text());
-             });
-
-            result =result + attackers +";"+sumAttackerLost+";"+sumDefenderLost;
-           }
+        var btnAnalyze = $('<input type="button" id="btnAnalyze" value="Analyze reports" />');
+        jQuery("div.boxes-contents.cf:first").append(btnAnalyze);
+        jQuery("#btnAnalyze").on('click', function () {
+            AnalyzeReports();
         });
-        no++;
-        var resultArray = result.split(";")
-        console.log("processed: "+no+" report");
-        $(this).parent().parent().parent().append('<td id="logAnalyzerTD">'+resultArray[0]+'</td>');
-        $(this).parent().parent().parent().append('<td id="logAnalyzerTD">'+resultArray[1]+'</td>');
-
-        //convert falsely values to zero: 0,NaN,undefined,false,....
-
-        resultArray[2] = isNaN(resultArray[2]) ? 0 : resultArray[2];
-        resultArray[3] = isNaN(resultArray[3]) ? 0 : resultArray[3];
-        var atkLostString= (resultArray[2] != 0) ? "Atk lost: "+resultArray[2] : "";
-        var defLostString= (resultArray[3] != 0) ? "Def lost: "+resultArray[3] : "";
-
-        $(this).parent().parent().parent().append('<td id="logAnalyzerTD">'+atkLostString+'</td>');
-        $(this).parent().parent().parent().append('<td id="logAnalyzerTD">'+defLostString+'</td>');
-
-        
-    })
-     }
 
 
+        //wrap jquery dom element in html and log
+        function logJqueryDOM(domElement) {
+            console.log(domElement.clone().wrap('<div/>').parent().html())
+        }
+
+        function AnalyzeReports() {
+            jQuery('td#logAnalyzerTD').each(function () {
+                $(this).remove();
+            });
+            var no = 0;
+            //minden kifosztotta, és támad linkre
+            jQ("a:contains('kifosztotta'),a:contains('támad')")
+                .each(function () {
+
+
+                    var linkToReport = $(this).attr('href');
+                    var result = {};
+                    jQuery.ajax({
+                        url: linkToReport,
+                        type: 'get',
+                        dataType: 'html',
+                        async: false,
+                        success: function (data) {
+                            var sumStolen = 0
+                            var parsedHtml = jQuery.parseHTML(data);
+
+                            //STOLEN RESOURCES
+                            jQuery("table#attacker tbody.goods img", parsedHtml).filter(".r1,.r2,.r3,.r4").each(function () {
+                                sumStolen += Number($(this).parent().text());
+                            });
+                            result.sumStolen = sumStolen;
+
+                            var attackers = [];
+                            var counter = 0;
+                            jQuery("table#attacker tbody.units:nth-child(3) td", parsedHtml).each(function () {
+                                if ($(this).text() != "0") {
+                                    var attackerObj = {};
+                                    attackerObj.iconHtml = jQuery("table#attacker tbody.units:nth-child(2) td:nth-child(" + (counter + 2) + ")", parsedHtml).html();
+                                    attackerObj.number = $(this).text();
+                                    attackers.push(attackerObj);
+                                }
+                                counter++;
+                            });
+                            result.attackers = attackers;
+
+
+                            //ATTACKER LOST
+                            var attackersLost = [];
+                            var counter = 0;
+                            var sumAttackerLost = 0
+                            jQuery("table#attacker tbody.units.last td", parsedHtml).each(function () {
+                                if ($(this).text() != "0") {
+                                    var attackerLostObj = {};
+                                    attackerLostObj.iconHtml = jQuery("table#attacker tbody.units:nth-child(2) td:nth-child(" + (counter + 2) + ")", parsedHtml).html();
+                                    attackerLostObj.number = $(this).text();
+                                    attackersLost.push(attackerLostObj);
+                                }
+                                sumAttackerLost += Number($(this).text());
+                                counter++;
+                            });
+                            result.attackersLost = attackersLost;
+                            result.sumAttackerLost = sumAttackerLost;
+
+                            //DEFENDERS LOST
+                            var sumDefenderLost = 0;
+                            jQuery("table#attacker", parsedHtml).siblings("table").find("tbody.units.last td").each(function () {
+                                sumDefenderLost += Number($(this).text());
+                            });
+                            result.sumDefenderLost = sumDefenderLost;
+                        }
+                    });
+                    no++;
+                    console.log("processed: " + no + " report");
+                    var parentToAppendTo = $(this).parent().parent().parent();
+                    parentToAppendTo.append('<td id="logAnalyzerTD">' + result.sumStolen + '</td>');
+
+
+                    //ATTACKERS
+                    var attackersHtmlToInsert = "";
+                    for (var i = 0; i < result.attackers.length; i++) {
+                        attackersHtmlToInsert = attackersHtmlToInsert + result.attackers[i].iconHtml + result.attackers[i].number + "</br>";
+                    }
+                    parentToAppendTo.append('<td id="logAnalyzerTD" width="15%">' + attackersHtmlToInsert + '</td>');
+
+
+                    console.log(result.attackersLost);
+                    //ATTACKERS LOST
+                    var attackersLostHtmlToInsert = "";
+                    for (var i = 0; i < result.attackersLost.length; i++) {
+                        attackersLostHtmlToInsert = attackersLostHtmlToInsert + result.attackersLost[i].iconHtml + result.attackersLost[i].number + "</br>";
+                    }
+                    parentToAppendTo.append('<td id="logAnalyzerTD" width="15%">' + attackersLostHtmlToInsert + '</td>');
 
 
 
-}
+                    // //convert falsely values to zero: 0,NaN,undefined,false,....
+                    // result.sumAttackerLost = isNaN(result.sumAttackerLost) ? 0 : result.sumAttackerLost;
+                    result.sumDefenderLost = isNaN(result.sumDefenderLost) ? 0 : result.sumDefenderLost;
+                    // var atkLostString = (result.sumAttackerLost != 0) ? "Atk lost: " + result.sumAttackerLost : "";
+                    var defLostString = (result.sumDefenderLost != 0) ? "Def lost: " + result.sumDefenderLost : "";
+                    //
+                    // $(this).parent().parent().parent().append('<td id="logAnalyzerTD">' + atkLostString + '</td>');
+                    $(this).parent().parent().parent().append('<td id="logAnalyzerTD">' + defLostString + '</td>');
+
+
+                })
+        }
+
+
+    }
 
 // load jQuery and execute the main function
-addJQuery(main);
-
+    addJQuery(main);
 
 
 })();
